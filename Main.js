520bd446da74b02d9d2d7b181eb1429ce0a99c8f
Mael <legrand.mael2002@gmail.com>
2021-01-13 10:48:55 +0100

    Story 1 Complète, Story 2 Complète, Story 3 Complète, Story 4 en cours, Story 5 en cours

--- Main.js
+++ Main.js
@@ -1,5 +1,6 @@
 // process.argv[2] - Récupère les arguments
 let args = process.argv;
+const { exception } = require('console');
 let fs = require('fs');
 
 
@@ -9,16 +10,24 @@ if(args[2] == '-help'){
 } else if (args[2] == "-action"){
   switch(args[3]){
     case "transform":
-      let start = new Date();
-      transformFile(args[4],args[5]);
-      let end = new Date();
-      console.log("Duration = " + (end - start) + "ms");
-      break;
+        let Tstart = new Date();
+        transformFile(args[4],args[5]);
+        let Tend = new Date();
+        console.log("Duration = " + (Tend - Tstart) + "ms");
+        break;
     case "sort_date":
+      let SDstart = new Date();
+      sortArrayByDate(args[4], args[5]);
+      let SDend = new Date();
+      console.log("Duration = " + (SDend - SDstart) + "ms");
       break;
     case "sort_title":
       break;
     case "search_date":
+      let S_Dstart = new Date();
+      searchDate(args[4],args[5],args[6]);
+      let S_Dend = new Date();
+      console.log("Duration = " + (S_Dend - S_Dstart) + "ms");
       break;
     case "search_key_word":
       break;
@@ -31,7 +40,7 @@ if(args[2] == '-help'){
 
 function timeConverter(timestamp){
     let a = new Date(timestamp * 1000);
-    let year = a.getFullYear()
+    let year = a.getFullYear();
     return year;
   }
 
@@ -43,10 +52,113 @@ function transformFile(input, output){
     allMovies.push(timestamp_date)
   }
   writeToFile(allMovies,output);
-  console.log('Saved!');
+  console.log('Everything Saved!');
 }
 
 function writeToFile(toWrite, output){
-  let writable = JSON.stringify(toWrite);
+  let writable = JSON.stringify(toWrite,null,"\t");
   fs.appendFileSync(output,writable);
+}
+
+function sortArrayByDate(input,output){
+  let file = require("./" + input);
+  let tempo = 0;
+  for(let i = 0; i < file.length; i++){ 
+    for(let j = i+1; j < file.length; j++){ // pour j allant de i+1 à tab.length
+      if(file[j].release_date < file[i].release_date){
+          tempo = file[i];
+          file[i] = file[j];
+          file[j] = tempo;
+      }
+    }
+  }
+  writeToFile(file,output);
+  console.log("Saved")
+}
+
+function searchDate(input, year, sorted){
+  let file = require("./" + input);
+  let dates = [];
+  if(sorted == "true"){
+    for(let j = 0;j<file.length;j++){
+      dates.push(timeConverter(file[j].release_date));
+    }
+    let sorted_array = tri_rapide(dates,0,dates.length-1);
+    console.log(searchBinaire(parseInt(year),sorted_array,0,sorted_array.length));
+
+  } else {
+    for(let i=0;i<file.length;i++){
+      let date = timeConverter(file[i].release_date);
+      if(date == year){
+        let donnee = file[i].title + " : " + "(" +date+")";
+        console.log(donnee);
+      }
+    }
+  }
+}
+
+function tri_alphabetique() {
+  let start = new Date();
+  let array = require('./movies.json')
+  let array2 = []
+  array2 = array
+  console.log(array2[0].title[0])
+  
+  for (let j = array2.length-1; j > 1; j--) {
+      for (let i = 0; i < (j-1); i++) {
+        
+          if (array2[j].title[0] < array2[i].title[0]) {
+              let temp = array2[j];
+              array2[j] = array2[i];
+              array2[i] = temp;
+          }
+      }
+  }
+  writeToFile(array2,"out.json")
+  let end = new Date();
+  console.log("Duration = " + (end - start) + "ms");
+  return array;
+}
+
+function searchBinaire(search, tableau, min, max) {
+  let index = Math.floor((min + max) / 2);
+
+  if (search === tableau[index]) {
+      return index;
+  }
+  if (search > tableau[index]) {
+      min = index;
+      return searchBinaire(search, tableau, min, max);
+  }
+  else {
+      max = index;
+      return searchBinaire(search, tableau, min, max);
+  }
+}
+
+function swap(tab,from,to){
+  let tmp = tab[to];
+  tab[to]= tab[from];
+  tab[from] = tmp;
+}
+function partitionner(t,premier,dernier,pivot){
+  swap(t,pivot,dernier);
+  j = premier;
+  for(i = premier;i<=dernier-1;i++){
+      if( t[i] <= t[dernier]){
+          swap(t,i,j)
+          j++;
+      }
+  }
+  swap(t,dernier,j)
+  return j;
+}
+function tri_rapide(t,premier, dernier){
+  if(premier<dernier){
+      let pivot = Math.ceil((premier + dernier) / 2);
+      pivot = partitionner(t,premier,dernier,pivot);
+      tri_rapide(t,premier, pivot-1);
+      tri_rapide(t,pivot+1,dernier);
+  }
+  return t;
 }
